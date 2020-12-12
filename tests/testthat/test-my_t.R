20a7cf7796783bd3a224f11cd26d6a06ad6e7b84
juanhillon <juanhillon37@gmail.com>
2020-12-11 22:41:43 -0800

    fix my_t.test tests

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -8,7 +8,10 @@ test_that("incorrect input for alternative throws error", {
   expect_error(my_t.test(x, 100, mu = 1))
 })
 
-test_that("output is a list", {
-  expect_is(my_t.test(my_gapminder$lifeExp, "less", mu = 60), "list")
+test_that("output is always a list", {
+  expect_is(my_t.test(1:10, "less", mu = 5), "list")
+  expect_is(my_t.test(1:10, "greater", mu = 5), "list")
+  expect_is(my_t.test(1:10, "two.sided", mu = 5), "list")
 })
+
 
