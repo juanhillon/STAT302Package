92baac29bb09ca6f2f2801a130090c3287d99386
juanhillon <juanhillon37@gmail.com>
2020-12-11 22:08:07 -0800

    Add output type test for my_t.test

--- tests/testthat/test-my_t.R
+++ tests/testthat/test-my_t.R
@@ -7,3 +7,8 @@ test_that("incorrect input for alternative throws error", {
   expect_error(my_t.test(x, "string", mu = 1))
   expect_error(my_t.test(x, 100, mu = 1))
 })
+
+test_that("output is a list", {
+  expect_is(my_t.test(my_gapminder$lifeExp, "less", mu = 60), "list")
+})
+
